732a69f6549f1e4e774468ef988a10eb8140945d
tokaygleam <tokay.gleam-8x@icloud.com>
2025-05-05 07:33:57 -0400

    Update calibrate_forward.ino

--- calibrate_forward/calibrate_forward.ino
+++ calibrate_forward/calibrate_forward.ino
@@ -13,28 +13,14 @@ const int TEST_DELAY_MS = 540;
 void setup() {
   Serial.begin(9600);
   while (!Serial);
-  Serial.println("---------------------------------");
-  Serial.print("Starting Forward Calibration for ");
-  Serial.print(TARGET_DISTANCE_CM);
-  Serial.println(" cm");
-  Serial.print("Testing Delay: ");
-  Serial.print(TEST_DELAY_MS);
-  Serial.println(" ms");
-  Serial.println("---------------------------------");
-
-
+  
   AppMotor.DeviceDriverSet_Motor_Init();
   delay(2000);
 
-  Serial.println("Moving forward...");
   ApplicationFunctionSet_SmartRobotCarMotionControl(Forward, 200);
   delay(TEST_DELAY_MS);
+  
   ApplicationFunctionSet_SmartRobotCarMotionControl(stop_it, 0);
-  Serial.println("Movement complete.");
-  Serial.print("Measure actual distance. Target was: ");
-  Serial.print(TARGET_DISTANCE_CM);
-  Serial.println(" cm");
-  Serial.println("Adjust TEST_DELAY_MS and re-run if needed.");
 }
 
 void loop() {
